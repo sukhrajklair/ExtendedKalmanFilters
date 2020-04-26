439c7c66bfe0206c4e111ec7b8d82ef71306a0f5
Sukhraj <sukhrajklair@gmail.com>
2020-04-26 10:35:09 -0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Desktop/workspace/ekf_lab/src
+# Build directory: /home/robond/Desktop/workspace/ekf_lab/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/robond/Desktop/workspace/ekf_lab/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/robond/Desktop/workspace/ekf_lab/devel;/opt/ros/kinetic".split(';') if "/home/robond/Desktop/workspace/ekf_lab/devel;/opt/ros/kinetic" != "" else []
